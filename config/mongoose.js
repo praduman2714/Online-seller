08cd4178f9edf5d384458f7772db42faffbffe2e
praduman2714 <pradu14jsr@gmail.com>
2023-06-09 14:12:55 +0530

    Added database to the cloud

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,12 @@
 const mongooese = require('mongoose');
 
-mongooese.connect('mongodb://127.0.0.1/onlineSeller');
+// mongooese.connect('mongodb://127.0.0.1/onlineSeller');
+const DB = 'mongodb+srv://whiteWolf:praduman@cluster0.b05xxvb.mongodb.net/?retryWrites=true&w=majority';
+
+// These set of line can be written in async await fashion, but I have followed the documentation. 
+mongooese.connect(DB).then(()=>{
+    console.log('connection successful');
+}).catch((err) => console.log("no connection " + err));
 
 const db = mongooese.connection;
 
